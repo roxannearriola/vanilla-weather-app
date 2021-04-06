12c76440539e5d5277720c3872649743743a67c4
roxannearriola <roxanne.arriola@gmail.com>
2021-04-06 01:46:22 -0400

    Updated to show current time

--- src/app.js
+++ src/app.js
@@ -1,11 +1,51 @@
+function formatDate(timestamp) {
+	let today = new Date(timestamp);
+	let hours = today.getHours();
+	if (hours < 10) {
+		hours = `0${hours}`;
+	}
+	let minutes = today.getMinutes();
+	if (minutes < 10) {
+		minutes = `0${minutes}`;
+	}
+	let days = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+	let day = days[today.getDay()];
+	let months = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	];
+	let month = months[today.getMonth()];
+	let date = today.getDate();
+	return `${day}, ${month} ${date} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
-	console.log(response.data);
 	let tempElement = document.querySelector("#current-temp");
 	let descriptionElement = document.querySelector("#current-temp-description");
 	let cityElement = document.querySelector("#city");
+	let dateElement = document.querySelector("#current-date");
 	tempElement.innerHTML = Math.round(response.data.main.temp);
 	descriptionElement.innerHTML = response.data.weather[0].description;
 	cityElement.innerHTML = response.data.name;
+	dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let city = "Montreal";
